5acf1a66f4d695d16f02314c21f210d667adbc19
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-21 15:40:06 +0100

    Esercizio completato

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <h1>La mia nuova APP con vue cli!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'newApp',
+  props: {
+    msg: String
+  }
+}
+</script>
